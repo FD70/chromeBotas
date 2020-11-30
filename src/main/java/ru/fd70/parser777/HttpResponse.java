0dd9b6b886fb9c48d854c2b12673ac75dc5e36b6
FD70000000 <55132316+FD70@users.noreply.github.com>
2020-11-30 12:59:14 +0300

    Update HttpResponse.java

--- src/main/java/ru/fd70/parser777/HttpResponse.java
+++ src/main/java/ru/fd70/parser777/HttpResponse.java
@@ -1,47 +1,33 @@
 package parser777;
 
-import initial.AFuncs;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
 import org.openqa.selenium.Cookie;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 
 public class HttpResponse {
     public static int codeViaGet(String url) {
         return RestAssured.get(url).statusCode();
     }
+    
+    public static int codeViaGet(String url, Set<org.openqa.selenium.Cookie> seleniumCookies) {
 
-    public static int codeViaGet(String url, Cookie token, Cookie ref_token) {
+        //адаптер, возможно нужно вынести из метода, чтобы каждый раз не буилдился
+        ArrayList<io.restassured.http.Cookie> restAssuredCookies = new ArrayList<>();
+
+        for (org.openqa.selenium.Cookie _cookie: seleniumCookies) {
+            restAssuredCookies.add(new io.restassured.http.Cookie.Builder(_cookie.toString()).build());
+        }
+
         return RestAssured.given()
-                .cookie(token.toString())
-                .cookie(ref_token.toString())
+                .cookies(new Cookies(restAssuredCookies))
                 .get(url).statusCode();
     }
-
-    public static String viaGetReturnHtmlFile(String url, Cookie token, Cookie ref_token){
-        return viaGetReturnTypeFile(url, ContentType.HTML, token, ref_token);
-    }
-
-    private static String viaGetReturnTypeFile(String url, ContentType _type, Cookie token, Cookie ref_token){
-        return RestAssured.given()
-                .cookie(token.toString())
-                .cookie(ref_token.toString())
-                .get(url).then().contentType(_type).extract().asString();
-    }
-
+    
     public static int codeViaPost(String url) {
         return RestAssured.post(url).statusCode();
-    }
-
-    public static void main(String[] args) {
-        String s = RestAssured
-                .get("http://yandex.ru/").getBody().prettyPrint();
-
-//        for (String hhh:Parser777.returnLinksFromHTML(s)) {
-//            System.out.println(hhh);
-//        }
-        System.out.println(s);
-        System.out.println(AFuncs._l);
-        System.out.println(Parser777.returnLinksFromHTML(s));
     }
 }
